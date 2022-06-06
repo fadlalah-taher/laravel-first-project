14a8ec5042d57b5361d83b0d07d1fb9016837fcb
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-06 15:40:32 +0300

    Adding TestController

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class TestController extends Controller{
+    
+    public function sayHi(){
+        echo "hi";
+    }
+    public function sayBye(){
+        echo "hi";
+    }
+
+
+
+}
