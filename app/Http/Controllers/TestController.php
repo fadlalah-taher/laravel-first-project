0fd8bb09211c772829f3b9429005e2b7ed8b19f5
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-06 17:21:23 +0300

    Adding function studentList

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,10 +5,6 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller{
     
-    public function sayHi(){
-        echo "hi";
-    }
-    
     public function palindrome(){
         $array = ['lks', 'tel', 'lol', 'abcba', 'north'];
         $count = 0;
@@ -26,11 +22,34 @@ class TestController extends Controller{
         echo $seconds;
     }
 
+    public function studentlist(){
+        $i = 0;
+        $j = 1;
+        $students=["Pablo","Nour","Mohamad","Pablo","Fouad","Hadi","Ali","Pablo","oula","eli","Pablo", "Hoda"];
+        for($x = 0; $x <= count($students)-1; $x++){
+            echo $students[$j];
+            echo " ";
+            echo $students[$i];
+            echo "<br>";
+            if($i+2 == count($students)-1 || $j+2 == count($students)-1){
+                $i = $i +2;
+                $j = $j +2;
+                echo $students[$j];
+                echo " ";
+                echo $students[$i];
+                echo "<br>";
+                return "finish";
+            }
+            $i = $i +2;
+            $j = $j +2;
+        }
+    }
 
 
     public function higherPro(){
         $array=["Pablo","Nour","Mohamad","Pablo","Fouad","Hadi","Ali","Pablo","oula","eli","Pablo"];
-        $result=rand(0,count($array));
+        $length = count($array)-1;
+        $result = rand(0, $length);
         echo $array[$result];
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 
 
-Route::get('/hi', [TestController::class, 'sayHi'])->name("say-hi");
+Route::get('/studentlist', [TestController::class, 'studentlist'])->name("studentlist");
 Route::get('/palindrome', [TestController::class, 'palindrome'])->name("palindome");
 Route::get('/time', [TestController::class, 'time'])->name("time");
 Route::get('/text', [TestController::class, 'text'])->name("text");
